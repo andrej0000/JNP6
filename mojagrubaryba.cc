3966f9eb8071ff7e2f9ba0bb7fdc5950e19e1881
Andrzej Sulecki <asulecki@gmail.com>
2014-01-23 00:15:34 +0100

    Fields without constructors

--- mojagrubaryba.cc
+++ mojagrubaryba.cc
@@ -7,6 +7,7 @@ void MojaGrubaRyba::setDie(std::shared_ptr<Die> die)
 
 MojaGrubaRyba::MojaGrubaRyba()
 {
+	//TODO chyba trzeba przerobic na factory jakies 
 	vector < Field > fields;
 	fields.push_back(new StartField);
 	fields.push_back(new PropertyField("Anemonia", 160));
@@ -15,10 +16,116 @@ MojaGrubaRyba::MojaGrubaRyba()
 	fields.push_back(new AquariumField(3));
 	fields.push_back(new PropertyField("Hotel w grocie", 300));
 	fields.push_back(new PropertyField("Menella", 280));
-	fields.push_back(new DeopsitField("Laguna", 15));
+	fields.push_back(new DepositField("Laguna", 15));
 	fields.push_back(new PropertyField("Statek", 250));
 	fields.push_back(new PrizeField("Blazenki", 120));
 	fields.push_back(new PropertyField("Pennatula", 400));
 	fields.push_back(new FineField("Rekin", 180));
 	board.fields = std::move(fields);
 }
+
+void StartField::action(shared_ptr< Player > p)
+{
+	p->addFishcoins(this->cash);
+}
+
+void StartField::pass(shared_ptr< Player > p)
+{
+	p->addFishcoins(this->cash);
+}
+
+
+void PrizeField::action(shared_ptr<Player> p)
+{
+	p->addFishcoins(this->ammount);
+}
+
+void PrizeField::pass(shared_ptr<Player> p)
+{
+	//nothing
+}
+
+
+void IslandField::action(shared_ptr<Player> p)
+{
+	//nothing
+}
+
+void IslandField::pass(shared_ptr<Player> p)
+{
+	//nothing
+}
+
+
+void PropertyField::action(shared_ptr<Player> p)
+{
+	if (this->owner.expired()){//noninitialized
+		if(p->wantBuy(this->name)){
+			this->owner = p;
+		}
+	} else if (auto owner = this->owner.lock()){
+		if (owner != p) {
+			int fare = (this->getPercent * this->price) / 100;
+			p->pay(fare);
+			owner->addFishcoins(fare);
+		}
+	}
+	else {
+		throw deletedPlayerException;//wysraj wyjatek
+	}
+}
+
+void PropertyField::pass(shared_ptr<Player> p)
+{
+
+}
+
+int PropertyField::getPrice(){
+	return this->price;
+}
+
+void PropertyField::sold(){
+	this->owner.reset();
+}
+
+int PublicPropertyField::getPercent()
+{
+	return 40;
+}
+
+int CoralPropertyField::getPercent()
+{
+	return 20;
+}
+
+void DepositField::action(shared_ptr<Player> p)
+{
+	p->addFishcoins(this->fishcoins);
+	this->fishcoins = 0;
+}
+
+void DepositField::pass(shared_ptr<Player> p)
+{
+	this->fishcoins += p->pay(this->ammount);
+}
+
+void FineField::action(shared_ptr<Player> p)
+{
+	this->fishcoins += p->pay(this->ammount);
+}
+
+void FineField::pass(shared_ptr<Player> p)
+{
+
+}
+
+void AquariumField::action(shared_ptr<Player> p)
+{
+	p->setWaitingTime(this->turnsToWait);
+}
+
+void AquariumField::pass(shared_ptr<Player> p)
+{
+
+}
+
--- mojagrubaryba.h
+++ mojagrubaryba.h
@@ -4,13 +4,21 @@
 #include "grubaryba.h"
 #include <string>
 #include <vector>
+#include <exception>
+using std::shared_ptr;
+using std::exception;
+using std::weak_ptr;
 
 class Player;
 class Board;
 class Field;
 
+
+
 class MojaGrubaRyba : public GrubaRyba
 {
+public:
+	void bankrupcy(shared_ptr< Player > p); //plejer zglasza bankructwo
 private:
 	static const int minPlayers = 2;
 	static const int maxPlayers = 8;
@@ -25,7 +33,22 @@ class Player
 private:
 	int pos;
 	int fishcoins;
+	int waitingTime;
+	std::vector< PropertyField > ownedProperties;
+public:
+	int pay(int ammount); //bankrutuje jesli nie ma kasy i cale konekwencje z tego wynikajace
+	void addFishcoins(int ammount);
+	void setWaitingTime(int wt);
+	virtual bool wantBuy(std::string const& propertyName) = 0;
+	virtual bool wantSell(std::string const& propertyName) = 0;
 };
+
+class deletedPlayerException : public Exception
+{
+	virtual const char* what() const throw(){
+		return "Player deleted prematurly";
+	}
+} deletedPlaterEx;
 
 class HumanPlayer : public Player
 {
@@ -46,50 +69,83 @@ class Board
 private:
 	std::vector < std::unique_ptr < Field > > fields;
 };
-
+//DONE
 class Field
 {
-	virtual void action() = 0;
-	virtual void pass() = 0;
+	virtual void action(shared_ptr<Player> p) = 0;
+	virtual void pass(shared_ptr<Player> p) = 0;
 protected:
 	const std::string name;
 };
 
 class StartField : public Field
 {
+	//TODO
+	// konstruktor
+	virtual void action(shared_ptr<Player> p) = 0;
+	virtual void pass(shared_ptr<Player> p) = 0;
+private:
+	int cash;	
 };
-
+//TODO Konstruktor 
 class PrizeField : public Field
 {
+	virtual void action(shared_ptr<Player> p) = 0;
+	virtual oid pass(shared_ptr<Player> p) = 0;
 private:
 	int ammount;
 };
-
+//TODO Konstruktor
 class FineField : public Field
 {
+	virtual void action(shared_ptr<Player> p) = 0;
+	virtual void pass(shared_ptr<Player> p) = 0;
 private:
 	int ammount;
 };
 
+//TODO Konstruktor
 class DepositField : public Field
 {
+	virtual void action(shared_ptr<Player> p) = 0;
+	virtual void pass(shared_ptr<Player> p) = 0;
 private:
 	const int ammount;
 	int fishcoins;
 };
 
-class Aquarium : public Field
+//TODO Konstruktor
+class AquariumField : public Field
 {
+	virtual void action(shared_ptr<Player> p) = 0;
+	virtual void pass(shared_ptr<Player> p) = 0;
 private:
 	const int turnsToWait;
 };
 
-
+//TODO konstruktor
 class PropertyField : public Field
 {
+public: 
+	virtual void action(shared_ptr< Player > p) = 0;
+	virtual void pass(shared_ptr< Player > p) = 0;
+	void sold();
+	int getPrice();
 private:
 	std::weak_ptr < Player > owner;
 	const std::string name;
+	int price;
+protected:
+	virtual int getPercent();
 };
-
+class PublicPropertyField : public PropertyField
+{
+protected:
+	int getPercent();
+};
+class CoralPropertyField : public PropertyField
+{
+protected:
+	int getPercent();
+};
 #endif
